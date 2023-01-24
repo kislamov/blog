59c45b624498d317f2778c4229e64f629147a756
Kamil <kislamovv@gmail.com>
2023-01-25 01:03:25 +0600

    Created new project. Adding base dependencies and started development

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+import { BrowserRouter as Router, Route, useHistory, Routes } from "react-router-dom";
+import {useState, useEffect} from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Header/>
+                <Nav/>
+                <Routes>
+                    <Route exact path='/' element={<Home/>}/>
+                    <Route exact path='post' element={<NewPost/>}/>
+                    <Route path='post/:id' element={<PostPage/>}/>
+                    <Route path='about' element={<About/>}/>
+                    <Route path='*' element={<Missing/>}/>
+                </Routes>
+                <Footer/>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
